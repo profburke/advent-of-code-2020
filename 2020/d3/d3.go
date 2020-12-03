16a7e0be8d0f40304d41dc45af07a733741679e6
Matthew M. Burke <matthew@bluedino.net>
2020-12-03 08:33:02 -0500

    day 3 part 2

--- 2020/d3/d3.go
+++ 2020/d3/d3.go
@@ -29,13 +29,11 @@ func readData() (trees [][]string) {
 	return
 }
 
-func main() {
-	trees := readData()
+func countCollisions(trees [][]string, slope Position) int {
 	rows := len(trees)
 	columns := len(trees[0])
 	treeCount := 0
 	position := Position{X: 0, Y: 0}
-	slope := Position{X: 1, Y: 3}
 
 	for position.X < rows {
 		if trees[position.X][position.Y] == "#" {
@@ -45,7 +43,37 @@ func main() {
 		position = position.addMod(slope, columns)
 	}
 
-	fmt.Println("Trees passed = ", treeCount)
+	return treeCount
+}
+
+func part1(trees [][]string) {
+	slope := Position{X: 1, Y: 3}
+
+	treeCount := countCollisions(trees, slope)
+	fmt.Println("Trees passed =", treeCount)
+}
+
+func part2(trees [][]string) {
+	product := 1
+	slopes := []Position{
+		Position{X: 1, Y: 1},
+		Position{X: 1, Y: 3},
+		Position{X: 1, Y: 5},
+		Position{X: 1, Y: 7},
+		Position{X: 2, Y: 1},
+	}
+
+	for _, slope := range slopes {
+		product *= countCollisions(trees, slope)
+	}
+	fmt.Println("Product of the collisions =", product)
+}
+
+func main() {
+	trees := readData()
+
+	part1(trees)
+	part2(trees)
 }
 
 // Local Variables:

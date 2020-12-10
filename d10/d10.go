f21b9c773c02135cd1228516c0f4b71f42fa97e9
Matthew M. Burke <matthew@bluedino.net>
2020-12-10 09:42:32 -0500

    whoops; delete dead code

--- d10/d10.go
+++ d10/d10.go
@@ -29,44 +29,6 @@ func removeAt(i int, list []int) (newList []int) {
 	return
 }
 
-type Entry struct {
-	Index   int
-	Joltage int
-}
-
-// func pickBest(adaptors []int, currentJoltage int) (index, joltage int, err error) {
-// 	oneJolts := make([]Entry, 0)
-// 	twoJolts := make([]Entry, 0)
-// 	threeJolts := make([]Entry, 0)
-
-// 	for index, joltage = range adaptors {
-// 		difference := adaptors[index] - currentJoltage
-
-// 		if difference == 1 {
-// 			oneJolts = append(oneJolts, Entry{Index: index, Joltage: joltage})
-// 		} else if difference == 2 {
-// 			twoJolts = append(twoJolts, Entry{Index: index, Joltage: joltage})
-// 		} else if difference == 3 {
-// 			threeJolts = append(threeJolts, Entry{Index: index, Joltage: joltage})
-// 		}
-// 	}
-
-// 	if len(oneJolts) > 0 {
-// 		index = oneJolts[0].Index
-// 		joltage = oneJolts[0].Joltage
-// 	} else if len(twoJolts) > 0 {
-// 		index = twoJolts[0].Index
-// 		joltage = twoJolts[0].Joltage
-// 	} else if len(threeJolts) > 0 {
-// 		index = threeJolts[0].Index
-// 		joltage = threeJolts[0].Joltage
-// 	} else {
-// 		err = errors.New("couldn't find a compatible adaptor")
-// 	}
-
-// 	return
-// }
-
 func part1(adaptors []int) {
 	currentJoltage := 0
 	oneJoltDifferences := 0
@@ -92,32 +54,13 @@ func part1(adaptors []int) {
 		currentJoltage = joltage
 	}
 
-	// for len(adaptors) > 0 {
-	// 	index, joltage, err := pickBest(adaptors, currentJoltage)
-	// 	if err != nil {
-	// 		// TODO
-	// 	}
-
-	// 	difference := joltage - currentJoltage
-
-	// 	if difference == 1 {
-	// 		oneJoltDifferences++
-	// 	} else if difference == 3 {
-	// 		threeJoltDifferences++
-	// 	} // difference could be 2 but we ignore it
-
-	// 	currentJoltage += joltage
-	// 	adaptors = removeAt(index, adaptors)
-	// }
-
-	// get max of adaptors + 3 to get joltage of your device
-	// take final currentJoltage and find difference between it and your device
-
 	threeJoltDifferences++ // for your device
 	fmt.Println("Part 1 =", oneJoltDifferences*threeJoltDifferences)
 }
 
 func part2(adaptors []int) {
+	count := 0
+	fmt.Println("Part 2=", count)
 }
 
 func main() {

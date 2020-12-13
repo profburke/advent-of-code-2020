35c7b68340e821274b02d07bd5158d646068f321
Matthew M. Burke <matthew@bluedino.net>
2020-12-13 11:12:06 -0500

    day 13 part 2 -- brute force got it; this is an unfinished attempt to be more clever

--- d13/d13.go
+++ d13/d13.go
@@ -19,11 +19,12 @@ func readData() (timestamp int, buses []int) {
 	line = scanner.Text()
 
 	for _, s := range strings.Split(line, ",") {
+		var bus int
 		if s == "x" {
-			continue
+			bus = -1
+		} else {
+			bus, _ = strconv.Atoi(s)
 		}
-
-		bus, _ := strconv.Atoi(s)
 		buses = append(buses, bus)
 	}
 
@@ -47,13 +48,75 @@ func part1(timestamp int, buses []int) {
 	fmt.Println(minWait, busId, minWait*busId)
 }
 
+func gcd(a, b int) int {
+	for b > 0 {
+		a, b = b, a%b
+	}
+
+	return a
+}
+
+func lcm(a, b int) int {
+	return a * b / gcd(a, b)
+}
+
+func lcmm(ns []int) (result int) {
+	if len(ns) == 1 {
+		return ns[0]
+	}
+
+	result = lcm(ns[0], ns[1])
+	for _, n := range ns[2:] {
+		result = lcm(result, n)
+	}
+
+	return
+}
+
+// this isn't working on the test files...
+// peeked at a coworker's solution, as best I can tell
+// he takes the opposite approach: i.e. take the lcm of
+// all the buses that do arrive at the proper time and
+// add that to timestamp
 func part2(timestamp int, buses []int) {
+	// ignore the passed in timestamp
+	//	timestamp = 100000000000000
+	timestamp = 0
+
+	for true {
+		offsets := make([]int, 0)
+		satisfies := true
+
+		for i, bus := range buses {
+			if bus == -1 {
+				continue
+			}
+
+			r := (timestamp + i) % bus
+			if r != 0 {
+				satisfies = false
+				s := bus - r
+				offsets = append(offsets, s)
+				//				break
+			}
+		}
+
+		if satisfies {
+			break
+		}
+
+		l := lcmm(offsets)
+		fmt.Println("timestamp", timestamp, "l", l)
+		timestamp += l
+	}
+
+	fmt.Println(timestamp)
 }
 
 func main() {
 	timestamp, buses := readData()
 
-	part1(timestamp, buses)
+	//	part1(timestamp, buses)
 	part2(timestamp, buses)
 }
 

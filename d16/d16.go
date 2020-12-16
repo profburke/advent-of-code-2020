6b8098bad325f50a76534abae440b610d8a5bc2f
Matthew M. Burke <matthew@bluedino.net>
2020-12-16 18:32:09 -0500

    day 16 part 2

--- d16/d16.go
+++ d16/d16.go
@@ -116,15 +116,65 @@ func part1(fields map[string]Field, tickets []Ticket) (validTickets []Ticket) {
 	return
 }
 
-func part2(fields map[string]Field, myTicket Ticket, Tickets []Ticket) {
-	// identifiedFieldNames := make([]string, len(fields))
+func GetPossibleColumns(field Field, tickets []Ticket) (columns []int) {
+	n := len(tickets[0])
+	for i := 0; i < n; i++ {
+		possible := true
+		for _, ticket := range tickets {
+			if !field.Valid(ticket[i]) {
+				possible = false
+				break
+			}
+		}
+
+		if possible {
+			columns = append(columns, i)
+		}
+	}
+
+	return
+}
+
+func RemoveFrom(l []int, c int) (nl []int) {
+	nl = make([]int, 0)
+	for _, v := range l {
+		if v != c {
+			nl = append(nl, v)
+		}
+	}
+	return
+}
+
+func part2(fields map[string]Field, myTicket Ticket, tickets []Ticket) {
+	// len(fields) == len(myTicket) -- otherwise there's an error
+	possibles := make(map[string][]int)
+
+	for name, field := range fields {
+		possibles[name] = GetPossibleColumns(field, tickets)
+	}
+
+	identifiedFieldNames := make(map[int]string)
+	for len(possibles) > 0 {
+		for name, list := range possibles {
+			if len(list) == 1 {
+				column := list[0]
+				identifiedFieldNames[column] = name
+				delete(possibles, name)
+				for n, l := range possibles {
+					possibles[n] = RemoveFrom(l, column)
+				}
+
+				break
+			}
+		}
+	}
 
 	product := 1
-	// for _, name := range identifiedFieldNames {
-	// 	if name[0] == 'd' {
-	// 		product *= fields[name]
-	// 	}
-	// }
+	for index, name := range identifiedFieldNames {
+		if len(name) > 8 && name[0:9] == "departure" {
+			product *= myTicket[index]
+		}
+	}
 
 	fmt.Println("Part 2 =", product)
 }

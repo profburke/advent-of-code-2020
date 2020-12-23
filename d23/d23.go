ac14c63a663496fc66ddf34a30afd7cf2ba69a2a
Matthew M. Burke <matthew@bluedino.net>
2020-12-23 08:15:05 -0500

    day 23 part 1

---
+++ d23/d23.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"container/ring"
+	"fmt"
+	"math"
+)
+
+func ToString(r *ring.Ring) string {
+	result := ""
+
+	r.Do(func(p interface{}) {
+		result += fmt.Sprintf("%d ", p.(int))
+	})
+
+	return result
+}
+
+func findDestination(r *ring.Ring, min, max int) (d *ring.Ring) {
+	label := r.Value.(int) - 1
+	if label < min {
+		label = max
+	}
+
+	for d == nil {
+		f := r
+		for true {
+			v := f.Value.(int)
+			if v == label {
+				d = f
+				return
+			}
+			f = f.Next()
+			if f == r {
+				break
+			}
+		}
+
+		label--
+		if label < min {
+			label = max
+		}
+	}
+
+	return
+}
+
+func part1(cups []int) {
+	min := math.MaxInt64
+	max := math.MinInt64
+	l := len(cups)
+	r := ring.New(l)
+
+	for i := 0; i < l; i++ {
+		r.Value = cups[i]
+		r = r.Next()
+	}
+
+	r.Do(func(p interface{}) {
+		v := p.(int)
+		if v > max {
+			max = v
+		}
+		if v < min {
+			min = v
+		}
+	})
+
+	fmt.Println(ToString(r))
+
+	for i := 0; i < 100; i++ {
+		// remove three cups to right of current
+		removed := r.Unlink(3)
+
+		// calculate destination
+		destination := findDestination(r, min, max)
+
+		// insert cups to right of destination
+		destination.Link(removed)
+
+		// adjust current
+		r = r.Next()
+
+		fmt.Println(ToString(r))
+	}
+
+	result := ""
+	r.Do(func(p interface{}) {
+		result += fmt.Sprintf("%d", p.(int))
+	})
+
+	fmt.Println(result)
+}
+
+func part2(cups []int) {
+}
+
+func main() {
+	cups := []int{1, 5, 6, 7, 9, 4, 8, 2, 3}
+
+	part1(cups)
+	part2(cups)
+}
+
+// Local Variables:
+// compile-command: "go build"
+// End:
